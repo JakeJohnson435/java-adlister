65d902e221a6f38e8a39660ef93424cfa8ced47a
Jake J <jakejohnson818@gmail.com>
2018-05-31 13:42:22 -0500

    Finished Servlets exercise as well as the bonuses.

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String reset = req.getParameter("reset");
+
+        res.setContentType("text/html");
+        counter += 1;
+        res.getWriter().println("<h1>Welcome to the page! You are visitor number " + counter + "!</h1>");
+        res.getWriter().println("<p>Add ?reset=yes to the url to reset the counter</p>");
+
+        if (reset.equals("yes")){
+            counter =0;
+        }
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userName = request.getParameter("name");
+        String pageNumber = request.getParameter("page");
+
+        if (!(userName == null)){
+            response.getWriter().println("<h1>Hello, " + userName + "!</h1>");
+        } else {
+            response.getWriter().println("<h1>Hello, World!</h1>");
+        }
+
+        response.getWriter().println("You're on page " + pageNumber);
     }
 }
+
+
